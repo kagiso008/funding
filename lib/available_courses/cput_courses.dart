734ebabb3df75f4f96693b9acc3ba0b73b7c48bc
kagiso008 <69651224+kagiso008@users.noreply.github.com>
2024-09-27 10:29:46 +0200

    test

--- lib/available_courses/cput_courses.dart
+++ lib/available_courses/cput_courses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:funding/pages/scholarshipsPage.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class CPUTCoursesPage extends StatefulWidget {
@@ -13,7 +14,7 @@ class CPUTCoursesPage extends StatefulWidget {
 class _CPUTCoursesPageState extends State<CPUTCoursesPage> {
   final SupabaseClient _supabaseClient = Supabase.instance.client;
   bool isLoading = true;
-  List<Map<String, dynamic>> availableCourses = [];
+  Map<String, List<Map<String, dynamic>>> facultyCourses = {};
 
   @override
   void initState() {
@@ -35,10 +36,10 @@ class _CPUTCoursesPageState extends State<CPUTCoursesPage> {
       final response = await _supabaseClient
           .from('universities')
           .select(
-              'university_name, qualification, aps, english_hl, english_fal, maths, technical_math, maths_lit, physical_sciences, life_orientation, accounting, business_studies, economics, history, geography, tourism, civil_technology, egd, cat, it, electrical_technology, mechanical_technology')
+              'university_name, qualification, aps, faculty, english_hl, english_fal, maths, technical_math, maths_lit, physical_sciences, life_orientation, accounting, business_studies, economics, history, geography, tourism, civil_technology, egd, cat, it, electrical_technology, mechanical_technology')
           .lte('aps', aps);
 
-      final List<Map<String, dynamic>> qualifiedCourses = [];
+      final Map<String, List<Map<String, dynamic>>> groupedCourses = {};
 
       // Mapping user subjects to university columns
       final subjectMapping = {
@@ -93,41 +94,64 @@ class _CPUTCoursesPageState extends State<CPUTCoursesPage> {
               (university['english_hl'] ?? 0)) {
             meetsRequirements = false;
           }
-        }
-
-        if (hasEnglishFAL) {
+        } else if (hasEnglishFAL) {
           if ((userMarks['first_additional_language_level'] ?? 0) <
               (university['english_fal'] ?? 0)) {
             meetsRequirements = false;
           }
+        } else {
+          meetsRequirements = false;
         }
 
-        // Compare other subjects (subject1, subject2, etc.)
-        for (int i = 1; i <= 4; i++) {
-          final subject = userMarks['subject$i'];
-          final subjectLevel = userMarks['subject${i}_level'];
+        /// Iterate over the subjects required by the university
+        subjectMapping.forEach((key, subjectName) {
+          // Get the required level for this subject from the university
+          final requiredLevel = university[key];
 
-          if (subject != null && subjectMapping.containsKey(subject)) {
-            final universitySubject = university[subjectMapping[subject]];
-            if ((subjectLevel ?? 0) < (universitySubject ?? 0)) {
+          // If the university has a requirement for this subject
+          if (requiredLevel != null) {
+            bool subjectFound = false;
+
+            // Check if any of the user's selected subjects match this university subject
+            for (int i = 1; i <= 4; i++) {
+              final userSubject = userMarks['subject$i'];
+              final userLevel = userMarks['subject${i}_level'];
+
+              // If the user's subject matches the university-required subject
+              if (userSubject == subjectName) {
+                subjectFound = true;
+
+                // If the user's subject level is less than the required level, they don't qualify
+                if ((userLevel ?? 0) < requiredLevel) {
+                  meetsRequirements = false;
+                  break;
+                }
+              }
+            }
+
+            // If the required subject wasn't found in the user's selected subjects
+            if (!subjectFound) {
               meetsRequirements = false;
-              break;
             }
           }
-        }
+        });
 
         // If requirements are met, add to the list
         if (meetsRequirements) {
-          qualifiedCourses.add(university);
+          final faculty = university['faculty'] ?? 'Unknown Faculty';
+          if (!groupedCourses.containsKey(faculty)) {
+            groupedCourses[faculty] = [];
+          }
+          groupedCourses[faculty]!.add(university);
         }
       }
 
       setState(() {
-        availableCourses = qualifiedCourses;
+        facultyCourses = groupedCourses;
         isLoading = false;
       });
 
-      if (availableCourses.isEmpty) {
+      if (facultyCourses.isEmpty) {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
               content: Text('No courses match your subjects and levels')),
@@ -146,50 +170,223 @@ class _CPUTCoursesPageState extends State<CPUTCoursesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: const Text("Courses you qualify for"),
+          backgroundColor: Colors.teal,
+        ),
+        body: isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : facultyCourses.isEmpty
+                ? const Center(child: Text('No courses available for your APS'))
+                : ListView.builder(
+                    padding: const EdgeInsets.all(16.0),
+                    itemCount: facultyCourses.keys.length,
+                    itemBuilder: (context, index) {
+                      final faculty = facultyCourses.keys.elementAt(index);
+                      final courses = facultyCourses[faculty]!;
+
+                      return Card(
+                        elevation: 4.0,
+                        margin: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: ExpansionTile(
+                          title: Text(
+                            faculty,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          children: courses.map((course) {
+                            return ListTile(
+                              title: Text(
+                                course['qualification'],
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              onTap: () {
+                                // Navigate to CoursePage when the ListTile is tapped
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => CoursePage(
+                                      course: course,
+                                      userAps: widget.aps,
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          }).toList(),
+                        ),
+                      );
+                    },
+                  ));
+  }
+}
+
+class CoursePage extends StatelessWidget {
+  final Map<String, dynamic> course;
+  final int userAps; // Add user's APS as a parameter
+
+  const CoursePage({super.key, required this.course, required this.userAps});
+
+  @override
+  Widget build(BuildContext context) {
+    // Subject mapping for easy display of required subjects
+    final Map<String, String> subjectMapping = {
+      'maths': 'Mathematics',
+      'maths_lit': 'Mathematical Literacy',
+      'technical_math': 'Technical Mathematics',
+      'english_hl': 'English Home Language',
+      'english_fal': 'English First Additional Language',
+      'physical_sciences': 'Physical Sciences',
+      'life_orientation': 'Life Orientation',
+      // Add more subjects as needed...
+    };
+
+    // Collect maths-related subjects and English subjects into separate lists
+    final List<Map<String, String>> mathSubjects = [];
+    final List<Map<String, String>> englishSubjects = [];
+    final List<Map<String, String>> otherSubjects = [];
+
+    subjectMapping.forEach((key, value) {
+      final subjectLevel = course[key];
+      if (subjectLevel != null && subjectLevel > 0) {
+        if (key == 'maths' || key == 'maths_lit' || key == 'technical_math') {
+          mathSubjects
+              .add({'subject': value, 'level': subjectLevel.toString()});
+        } else if (key == 'english_hl' || key == 'english_fal') {
+          englishSubjects
+              .add({'subject': value, 'level': subjectLevel.toString()});
+        } else {
+          otherSubjects
+              .add({'subject': value, 'level': subjectLevel.toString()});
+        }
+      }
+    });
+
+    return Scaffold(
       appBar: AppBar(
-        title: const Text("Courses you qualify for"),
+        title: Text(course['qualification'] ?? 'Course Details'),
         backgroundColor: Colors.teal,
       ),
-      body: isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : availableCourses.isEmpty
-              ? const Center(child: Text('No courses available for your APS'))
-              : ListView.builder(
-                  padding: const EdgeInsets.all(16.0),
-                  itemCount: availableCourses.length,
-                  itemBuilder: (context, index) {
-                    final course = availableCourses[index];
-                    return Card(
-                      elevation: 4.0,
-                      margin: const EdgeInsets.symmetric(vertical: 10.0),
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              course['qualification'],
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              'University: ${course['university_name']}',
-                              style: const TextStyle(fontSize: 16),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              'APS Required: ${course['aps']}',
-                              style: const TextStyle(fontSize: 16),
-                            ),
-                          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              course['qualification'] ?? 'Unknown Qualification',
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.teal,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Faculty: ${course['faculty'] ?? 'Unknown Faculty'}',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'APS Requirement: ${course['aps'] ?? 'Not Available'}',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Your APS: $userAps', // Display user's APS
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.deepOrangeAccent,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Divider(),
+            const SizedBox(height: 10),
+            Text(
+              'Required Subjects & Levels',
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.teal,
+              ),
+            ),
+            const SizedBox(height: 10),
+            // Display maths-related subjects with 'or' between them
+            if (mathSubjects.isNotEmpty) ...[
+              Card(
+                elevation: 4.0,
+                margin: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ListTile(
+                  leading: const Icon(Icons.book, color: Colors.teal),
+                  title: Text(
+                    mathSubjects
+                        .map((subj) =>
+                            '${subj['subject']} (Level ${subj['level']})')
+                        .join(' or '),
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+            // Display English-related subjects with 'or' between them
+            if (englishSubjects.isNotEmpty) ...[
+              Card(
+                elevation: 4.0,
+                margin: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ListTile(
+                  leading: const Icon(Icons.book, color: Colors.teal),
+                  title: Text(
+                    englishSubjects
+                        .map((subj) =>
+                            '${subj['subject']} (Level ${subj['level']})')
+                        .join(' or '),
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+            // Display other required subjects
+            Expanded(
+              child: ListView.builder(
+                itemCount: otherSubjects.length,
+                itemBuilder: (context, index) {
+                  final subject = otherSubjects[index];
+                  return Card(
+                    elevation: 4.0,
+                    margin: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: ListTile(
+                      leading: const Icon(Icons.book, color: Colors.teal),
+                      title: Text(
+                        subject['subject']!,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    );
-                  },
-                ),
+                      subtitle: Text(
+                        'Required Level: ${subject['level']}',
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
     );
   }
 }
